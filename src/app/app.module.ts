eb068a2b17df0a0b4f3f0caacfe779120ee54b0a
cristtian177 <cristtian177@gmail.com>
2022-11-07 16:58:45 -0500

    create banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarouselComponent } from './components/carousel-item/carousel.component';
 import { HomeComponent } from './routes/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarouselComponent,
     HomeComponent,
+    NavbarComponent,
+    BannerComponent,
   ],
   imports: [
     BrowserModule,
